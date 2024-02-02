e1439448037cc4868ddc6818813c70e97e389351
metehandemirkol <demirkolmetehan@hotmail.com>
2024-02-01 20:25:51 -0800

    Create columns,image and description

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Metehan Demirkol")
+    content = """
+    Hi, I am Metehan! I am Python programmer,and GIS Analyst. I graduated in 2015 in Survey Engineer field.I have
+    worked with companies from various countries.
+    
+    """
+
+    st.info(content)
